caaf32246ec41b6d267d1fa4a5d0e2cafe230a1d
Kevin Peters <kevin@wormholelabs.xyz>
2025-09-25 18:21:25 -0500

    fix imports

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,37 @@
-import './App.css';
+import "./App.css";
 import WormholeConnect, {
-	type config,
-	WormholeConnectTheme,
-	cctpExecutorRoute as getCctpExecutorRoute,
-	cctpV2FastExecutorRoute as getCctpV2FastExecutorRoute,
-	cctpV2StandardExecutorRoute as getCctpV2StandardExecutorRoute,
-} from '@wormhole-foundation/wormhole-connect';
+  type config,
+  WormholeConnectTheme,
+} from "@wormhole-foundation/wormhole-connect";
+import {
+  cctpExecutorRoute,
+  cctpV2FastExecutorRoute,
+  cctpV2StandardExecutorRoute,
+} from "@wormhole-foundation/wormhole-connect/executor";
 
 function App() {
-	const config: config.WormholeConnectConfig = {
-		// Define the network
-		network: 'Mainnet',
-		routes: [
-			getCctpExecutorRoute,
-			getCctpV2FastExecutorRoute,
-			getCctpV2StandardExecutorRoute
-		],
+  const config: config.WormholeConnectConfig = {
+    // Define the network
+    network: "Mainnet",
+    routes: [
+      cctpExecutorRoute(),
+      cctpV2FastExecutorRoute(),
+      cctpV2StandardExecutorRoute(),
+    ],
 
-		// Define the chains
-		chains: ['Sui', 'Sonic'],
-		ui: {
-			title: 'SUI Connect TS Demo',
-		},
-	};
+    // Define the chains
+    chains: ["Sui", "Sonic"],
+    ui: {
+      title: "SUI Connect TS Demo",
+    },
+  };
 
-	const theme: WormholeConnectTheme = {
-		mode: 'dark',
-		primary: '#78c4b6',
-	};
+  const theme: WormholeConnectTheme = {
+    mode: "dark",
+    primary: "#78c4b6",
+  };
 
-	return <WormholeConnect config={config} theme={theme} />;
+  return <WormholeConnect config={config} theme={theme} />;
 }
 
 export default App;
